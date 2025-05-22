10110472dae9dc5ba241f5a2d7b091ca08dd5bd2
Chaudharikomal24 <chaudharikomal394@gmail.com>
2025-05-22 23:37:13 +0530

    Add files via upload

---
+++ Todo+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Todo+CoreDataClass.swift
+//  KomalTodo project
+//
+//  Created by Felix IT on 17/4/2025
+//  Copyright © 2022 Felix IT. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Todo)
+public class Todo: NSManagedObject {
+
+}
